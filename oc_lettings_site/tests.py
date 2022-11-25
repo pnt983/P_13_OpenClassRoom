bfc644ea7aba531ce78a35cfa38826f5995832f1
pnt983 <82818082+pnt983@users.noreply.github.com>
2022-11-25 11:52:20 +0100

    Ajout des tests pour oc_lettings_site

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,8 +1,15 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
 
 
-client = Client()
+@pytest.mark.django_db
+def test_index():
+    client = Client()
+    path = reverse('index')
+    response = client.get(path)
+    content = response.content.decode()
+    expected_value = "<h1>Welcome to Holiday Homes</h1>"
 
-
-def test_dummy():
-    assert 1
+    assert response.status_code == 200
+    assert expected_value in content
