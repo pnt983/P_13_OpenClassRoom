4f6c0d08efe88e7a66fbaf46d1d14da3ca950a96
pnt983 <82818082+pnt983@users.noreply.github.com>
2022-11-25 14:52:52 +0100

    Modification de allwoed_host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'oc-lettings-klm.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'oc-lettings-klm.herokuapp.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
