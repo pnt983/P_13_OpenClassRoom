b66846053649976040bb208609cc53be5e677a7a
pnt983 <82818082+pnt983@users.noreply.github.com>
2022-12-01 11:24:20 +0100

    Ajout des staticsfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'profiles',
     'lettings',
 ]
