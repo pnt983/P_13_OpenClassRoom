bb23ba58cbac6f1bfc69ec9a1813b29a7cc4c1b0
pnt983 <82818082+pnt983@users.noreply.github.com>
2022-12-02 15:20:21 +0100

    Ajout des commentaires aux fonctions

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -9,29 +9,3 @@ from django.shortcuts import render
 # Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
-
-# Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat
-# massa. Integer est nunc, pulvinar a tempor et, bibendum id arcu. Vestibulum ante ipsum primis in
-# faucibus orci luctus et ultrices posuere cubilia curae; Cras eget scelerisque
-
-
-# Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id
-# eleifend. Praesent dignissim, odio eu consequat pretium, purus urna vulputate arcu, vitae
-# efficitur lacus justo nec purus. Aenean finibus faucibus lectus at porta. Maecenas auctor, est
-# ut luctus congue, dui enim mattis enim, ac condimentum velit libero in magna. Suspendisse
-# potenti. In tempus a nisi sed laoreet. Suspendisse porta dui eget sem accumsan interdum. Ut quis
-# urna pellentesque justo mattis ullamcorper ac non tellus. In tristique mauris eu velit fermentum,
-# tempus pharetra est luctus. Vivamus consequat aliquam libero, eget bibendum lorem. Sed non dolor
-# risus. Mauris condimentum auctor elementum. Donec quis nisi ligula. Integer vehicula tincidunt
-# enim, ac lacinia augue pulvinar sit amet.
-
-
-# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
-# pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
-# lacus d
-
-
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor
-# id facilisis fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque
-# habitant morbi tristique senectus et netus et males
